55afed7f6156af0f04a3ab2ceb4044856a258818
sato_atsu <58408899+sat0atsu@users.noreply.github.com>
2023-03-28 19:04:05 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,37 +1,75 @@
 const fileInput = document.getElementById('fileInput');
 const modifyButton = document.getElementById('modify');
 const downloadButton = document.getElementById('download');
+const categoriesTextarea = document.getElementById('categories');
+const itemsTextarea = document.getElementById('items');
 let modifiedFileContent;
 
-fileInput.addEventListener('change', function() {
-    downloadButton.disabled = true;
-});
+fileInput.addEventListener('change', function () {
+  downloadButton.disabled = true;
 
-modifyButton.addEventListener('click', function() {
-    const searchText = document.getElementById('searchText').value;
-    const replaceText = document.getElementById('replaceText').value;
+  const file = fileInput.files[0];
+  const reader = new FileReader();
 
-    if (!fileInput.files.length || !searchText || !replaceText) {
-        alert('ファイルを選択し、検索テキストと置換テキストを入力してください');
-        return;
+  reader.onload = function (e) {
+    const content = e.target.result;
+    const categoriesMatch = content.match(/var categories = \["(.*?)"\];/);
+    const itemsMatch = content.match(/"bg": \["(.*?)"\]/);
+
+    if (categoriesMatch && categoriesMatch[1]) {
+      categoriesTextarea.value = categoriesMatch[1];
+      categoriesTextarea.disabled = false;
     }
 
-    const file = fileInput.files[0];
-    const reader = new FileReader();
-
-    reader.onload = function(e) {
-        const content = e.target.result;
-        modifiedFileContent = content.replace(new RegExp(searchText, 'g'), replaceText);
-        downloadButton.disabled = false;
-    };
-
-    reader.readAsText(file);
-});
-
-downloadButton.addEventListener('click', function() {
-    if (!modifiedFileContent) {
-        alert('まずファイルを書き換えてください');
-        return;
+    if (itemsMatch && itemsMatch[1]) {
+      itemsTextarea.value = itemsMatch[1];
+      itemsTextarea.disabled = false;
     }
 
-    const blob = new Blob([modifiedFileContent],
+    modifiedFileContent = content;
+  };
+
+  reader.readAsText(file);
+});
+
+modifyButton.addEventListener('click', function () {
+  if (!fileInput.files.length) {
+    alert('ファイルを選択してください');
+    return;
+  }
+
+  modifiedFileContent = modifiedFileContent.replace(
+    /var categories = \["(.*?)"\];/,
+    `var categories = ["${categoriesTextarea.value}"];`
+  );
+
+  modifiedFileContent = modifiedFileContent.replace(
+    /"bg": \["(.*?)"\]/,
+    `"bg": ["${itemsTextarea.value}"]`
+  );
+
+  downloadButton.disabled = false;
+});
+
+downloadButton.addEventListener('click', function () {
+  if (!modifiedFileContent) {
+    alert('まずファイルを書き換えてください');
+    return;
+  }
+
+  const blob = new Blob([modifiedFileContent], { type: 'text/plain;charset=utf-8' });
+  const url = URL.createObjectURL(blob);
+
+  const link = document.createElement('a');
+  link.href = url;
+  link.download = fileInput.files[0].name;
+  link.style.display = 'none';
+
+  document.body.appendChild(link);
+  link.click();
+
+  setTimeout(() => {
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
+  }, 100);
+});
