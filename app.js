fd70b3f856e71caeb3eed7eb348968317cffdf48
sato_atsu <58408899+sat0atsu@users.noreply.github.com>
2023-03-28 18:29:26 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
-document.getElementById('convert').addEventListener('click', function() {
-    const input = document.getElementById('input').value;
-    const words = input.split('　');
+const fileInput = document.getElementById('fileInput');
+const modifyButton = document.getElementById('modify');
+const downloadButton = document.getElementById('download');
+let modifiedFileContent;
 
-    const output = words.map(word => `"${word.trim()}"`).join(', ');
-
-    document.getElementById('output').value = output;
+fileInput.addEventListener('change', function() {
+    downloadButton.disabled = true;
 });
+
+modifyButton.addEventListener('click', function() {
+    const searchText = document.getElementById('searchText').value;
+    const replaceText = document.getElementById('replaceText').value;
+
+    if (!fileInput.files.length || !searchText || !replaceText) {
+        alert('ファイルを選択し、検索テキストと置換テキストを入力してください');
+        return;
+    }
+
+    const file = fileInput.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function(e) {
+        const content = e.target.result;
+        modifiedFileContent = content.replace(new RegExp(searchText, 'g'), replaceText);
+        downloadButton.disabled = false;
+    };
+
+    reader.readAsText(file);
+});
+
+downloadButton.addEventListener('click', function() {
+    if (!modifiedFileContent) {
+        alert('まずファイルを書き換えてください');
+        return;
+    }
+
+    const blob = new Blob([modifiedFileContent],
